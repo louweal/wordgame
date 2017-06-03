302815f3b8f71311d3a9e8ce66379418c7326169
louweal <anneloeslouwe@live.nl>
2017-06-03 12:22:30 +0200

    update spider + add scraped data

--- learn-english/scrape/spiders/learn-english-spider.py
+++ learn-english/scrape/spiders/learn-english-spider.py
@@ -2,15 +2,17 @@ import scrapy
 
 
 class WordsSpider(scrapy.Spider):
-	name = "learn-english" #this name is used to run the spiders
+	name = "learn-english" 
 
 	def start_requests(self):
         	urls = []
-		for i in range(1, 4):
+		for i in range(1, 220):
 			urls.append('http://learn-english-forum.org/discussion/13/word-association/p'+str(i))
 			
 		for url in urls:
 			yield scrapy.Request(url=url, callback=self.parse)
+
+
         
 	def parse(self, response):
          for item in response.css('div.Comment'):
