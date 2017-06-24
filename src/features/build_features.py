9d732f9806d22bbc6afc968ff90dcabf67a43699
louweal <anneloeslouwe@live.nl>
2017-06-24 18:54:09 +0200

    added plot levensthein distance

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -7,14 +7,21 @@ Basic feature extraction script
 """
 
 
-import Levenshtein
 import pandas as pd
 import numpy as np
+import matplotlib as plt
+import seaborn as sb
+
+import Levenshtein
+
 
 def levenshtein(r):
-#	return 1
+	return 1
 	return Levenshtein.distance(r.word1, r.word2)
 
+#
+def isNt(r):
+	return ((r.sourceID%9)>0)
 
 #read input data
 infile = pd.read_csv('../../data/processed/wordgame_201706.csv')
@@ -22,6 +29,7 @@ infile = pd.read_csv('../../data/processed/wordgame_201706.csv')
 #create new dataframe
 df = pd.DataFrame(infile)
 df['sourceID'] = df['source'].astype('category').cat.codes	
+df['nt'] = df.apply(isNt, axis=1)
 
 df['word1'] = df['word1'].map(str)
 df['word2'] = df['word2'].map(str)
@@ -34,15 +42,43 @@ print("Min word length: "+ str(df['len2'].min()))
 print("Max word length: "+ str(df['len2'].max()))
 print("Mean word length: "+ str(df['len2'].mean()))
 
+#Plot word lengths
+
+#Remove outliers 
+#df = ...
+
+
 df['edit'] = df.apply(levenshtein, axis=1)
+
 
 print("Mean edit distance: "+ str(df['edit'].mean()))
 
-#subset common word lengths
-sdf = df[(2 < df['len1']) & (df['len1'] < 18) & (2 < df['len2']) & (df['len2'] < 18)]
+#delete outliers in terms of word length
+df = df[(2 < df['len1']) & (df['len1'] < 18) & (2 < df['len2']) & (df['len2'] < 18)]
+
+nt = df[(df['sourceID'] != 9) & (df['sourceID'] != 0)]
+asd = df[(df['sourceID'] == 9) | (df['sourceID'] == 0)]
+
+#normalize?
+#nt['edit'] = nt['edit']/nt['edit'].max().astype(np.float64)
+#asd['edit'] = asd['edit']/asd['edit'].max().astype(np.float64)
 
 
-med = np.zeros(shape=(10,1)) # mean edit distance difference 
+weights_nt = np.ones_like(nt['edit'])/len(nt['edit'])
+weights_asd = np.ones_like(asd['edit'])/len(asd['edit'])
+
+nt['edit'].plot.hist(by='edit', bins=15, alpha=0.5, label='NT', weights=weights_nt)
+asd['edit'].plot.hist(by='edit', bins=15, alpha=0.5, label='ASD', weights=weights_asd)
+#plt.legend(loc='upper right')
+
+
+
+
+
+
+
+
+edit = np.zeros(shape=(10,1)) # mean edit distance difference 
 
 df10 = pd.DataFrame()
 df10 = df10.append(pd.DataFrame([0,1, 2,3,4,5,6,7,8,9]))
@@ -50,14 +86,46 @@ df10 = df10.append(pd.DataFrame([0,1, 2,3,4,5,6,7,8,9]))
 
 for i in range(0,10):
 	small = sdf[(sdf['sourceID'] == i)]
-	med[i,0] = small['edit'].mean()
+	edit[i,0] = small['edit'].mean()
 		
 
 
 np.set_printoptions(precision=4)
-print(med)		
+print(edit)		
 
-'''
+''''
+#Word2Vec
+from gensim.models.keyedvectors import KeyedVectors
+
+
+print('Loading word embeddings...')
+w2v_model = KeyedVectors.load_word2vec_format('../../data/external/GoogleNews-vectors-negative300.bin', binary=True)
+
+def similarity(r):
+    found       = False
+    normalized  = False
+    word_vector = None
+
+    # If word in vocab as-is vectorize
+    if r.word1 in w2v_model.vocab:
+        found       = True
+        word_vector = w2v_model.word_vec(word)
+	
+	word_vectors.similarity(r.word1, r.word2)
+	return word_vector
+
+for r in df:
+	
+df['sim'] = df.apply(similarity, axis=1) 
+
+
+
+
+
+
+
+
+
 ppa = []
 maxdup = []
 meandup = []
