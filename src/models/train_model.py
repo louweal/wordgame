cf7e274e53ef2cd806efde02c701ef0670da0714
louweal <anneloeslouwe@live.nl>
2017-08-12 16:07:28 +0200

    created CNN model and LSTM model +data

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -2,57 +2,28 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from keras.models import Model
-from keras.layers import Dense, Conv2D, MaxPooling2D, Input, Flatten, Dropout
+from keras.layers import Dense, Conv2D, MaxPooling2D, Input, Flatten, Dropout, LSTM
 from keras.layers.merge import Concatenate, Dot
 from keras.callbacks import ModelCheckpoint
-
+from gensim.models.keyedvectors import KeyedVectors
  
-df = pd.read_csv('../../data/processed/wordgame_20170807.csv')
+df = pd.read_csv('../../data/processed/wordgame_train.csv')
 df['word'] = df['word'].astype(str)
 df['association'] = df['association'].astype(str)
 
-df['asd'] = df['forumID'].apply(lambda x:(x%9==0))
-
-df['word_count1'] = df['word'].apply(lambda x:(x.replace('  ',' ').count(' ')+1))
-df = df[df['word_count1']==1]
-df['word_count2'] = df['association'].apply(lambda x:(x.replace('  ',' ').count(' ')+1))
-df = df[df['word_count2']==1]
-len(df)
-
-#X1_train = np.zeros(shape=(len(df), 1, 300), dtype=np.float16)
-#X2_train = np.zeros(shape=(len(df), 1, 300), dtype=np.float16)
-#y_train = np.zeros(shape=(len(df)), dtype=np.int8)
-
-from gensim.models.keyedvectors import KeyedVectors
 w2v_model = KeyedVectors.load_word2vec_format('../../data/external/GoogleNews-vectors-negative300.bin', binary=True)
 print('Loaded word embeddings')
 
-df['inw2v'] = df.apply(lambda r:((r.word in w2v_model.vocab) & (r.association in w2v_model.vocab)), axis=1)
-print("Mean: "+str(df.inw2v.mean()))
-
-#compute similarity
-#df['sim'] = 0
-#df.ix[df.inw2v, 'sim'] = df.ix[df.inw2v].apply(lambda r:w2v_model.similarity(r.word, r.association), axis=1)
-
-# create word vectors
-df['wv1'] = df['word'].apply(lambda x: np.zeros(300)) 
-df['wv2'] = df['word'].apply(lambda x: np.zeros(300)) 
-
-df.ix[df.inw2v, 'wv1'] = df.ix[df.inw2v, 'word'].apply(lambda x: w2v_model.word_vec(x)) 
-df.ix[df.inw2v, 'wv2'] = df.ix[df.inw2v, 'association'].apply(lambda x: w2v_model.word_vec(x)) 
-
-# pandas yes? 
-X1_train = df['wv1'].tolist()
-X2_train = df['wv2'].tolist()
+X1_train = df['word'].apply(lambda x: w2v_model.word_vec(x)).tolist() 
+X2_train = df['association'].apply(lambda x: w2v_model.word_vec(x)).tolist() 
 y_train = df['asd']
-
-w2v_model = None #memory!
 
 #reshape data for some reason..
 X1_train = np.reshape(X1_train, newshape=(len(X1_train), 1, 300, 1))
 X2_train = np.reshape(X2_train, newshape=(len(X2_train), 1, 300, 1))
 y_train  = np.reshape(y_train,  newshape=(len(y_train),  1))
 
+#w2v_model = None #memory!
 
 #The Keras Network
 print("Building neural network...")
@@ -62,14 +33,20 @@ convolution = Conv2D(name='convolution', filters=100, kernel_size=(1,300),paddin
 pooled_output  = MaxPooling2D(name='pooled_output', pool_size=(1, 1))(convolution)
 pooled_output  = Flatten(name='pooled_output_flat')(pooled_output)
 pooled_output  = Dropout(name='some_dropout', rate=0.25)(pooled_output)
-shared_cnn     = Model(name='shared_cnn', inputs=sentence_input, outputs=pooled_output)
+#shared_cnn     = Model(name='shared_cnn', inputs=sentence_input, outputs=pooled_output)
+
+#LSTM
+sentence_input = Input(name='sentence_input', shape=(1,300)) # (timestep,input_dim)
+lstm = LSTM(name='LSTM', units=225, activation='tanh', recurrent_activation='hard_sigmoid', dropout=0.15, recurrent_dropout=0.15)(sentence_input)
+shared_cnn = Model(name='shared_lstm', inputs=sentence_input, outputs=lstm) #actually lstm ofcourse
+
 
 print("shared_cnn summary:")
 print(shared_cnn.summary())
 print("\n")
 
-input_1 = Input(shape=(1,300,1), name='word1_input') # (height, width, depth)
-input_2 = Input(shape=(1,300,1), name='word2_input')
+input_1 = shared_cnn(Input(shape=(1,300,1), name='word1_input')) # (height, width, depth)
+input_2 = shared_cnn(Input(shape=(1,300,1), name='word2_input'))
 
 shared_cnn1 = shared_cnn(input_1)
 shared_cnn2 = shared_cnn(input_2)
@@ -91,17 +68,37 @@ print("\n")
 print("Training model...")
 # Saving best versions of network
 
-checkpoint = ModelCheckpoint('cnn_best_weights2.hdf5', monitor='val_loss', verbose=0, save_best_only=True, mode='min')
-#callbacks_list = [checkpoint]
+checkpoint = ModelCheckpoint('lstm.hdf5', monitor='val_loss', verbose=0, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+#ifif
 model.fit(x=[X1_train, X2_train], y=y_train, batch_size=50, epochs=10, validation_split=0.1, callbacks=[checkpoint])
-          # also validate at the same time
 
 # Load best found weights
 print("Loading best found model...")
-model.load_weights('cnn_best_weights2.hdf5')
+model.load_weights('lstm.hdf5')
 # Just some validation that everything works
 (loss, accuracy) = model.evaluate(x=[X1_train, X2_train], y=y_train, verbose=1)
 print(" - loss: {} - acc: {}".format(loss, accuracy))
+
+###
+
+dft = pd.read_csv('../../data/processed/wordgame_test.csv')
+dft['word'] = dft['word'].astype(str)
+dft['association'] = dft['association'].astype(str)
+
+X1_test = dft['word'].apply(lambda x: w2v_model.word_vec(x)).tolist() 
+X2_test = dft['association'].apply(lambda x: w2v_model.word_vec(x)).tolist() 
+y_test = dft['asd']
+
+#reshape data for some reason..
+X1_test = np.reshape(X1_test, newshape=(len(X1_test), 1, 300, 1)) #combine
+X2_test = np.reshape(X2_test, newshape=(len(X2_test), 1, 300, 1))
+y_test  = np.reshape(y_test,  newshape=(len(y_test),  1))
+
+predictions = model.predict(x=[X1_test, X2_test])
+dft['pred'] = predictions.flatten()
+	
+dft.to_csv("../../data/processed/wordgame_test_lstm.csv", sep=',', index=False)
 
 
 
